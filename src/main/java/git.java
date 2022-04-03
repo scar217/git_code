7b3d8597cf6ad863d34f43ef6a5081b2d2593072
scar217 <73574679+scar217@users.noreply.github.com>
2022-04-03 18:38:08 +0800

    Update git.java

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -6,5 +6,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("2022.4.3");
+        System.out.println("2022.4.3.18:37");
     }
 }
